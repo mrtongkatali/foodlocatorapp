9d9a378a7b468cebe74b37bf0128ae637bb7c22b
Leo Diaz <leoangelo.diaz@gmail.com>
2016-02-03 20:15:26 +0800

    .:

--- controllers/demo.js
+++ controllers/demo.js
@@ -1,7 +1,21 @@
-var appControllers = angular.module('appControllers', []);
+var appControllers = angular.module('appControllers', ['ui.bootstrap','uiGmapgoogle-maps',]);
 
 
-appControllers.controller('demoCtrl', ['$scope','$timeout','$routeParams',
-  function($scope, $timeout, $routeParams) {
+appControllers.config(['uiGmapGoogleMapApiProvider',
+    function(uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            key: 'AIzaSyA6Sv1tA7RxdhTVjw0UisBmBTwTR1B',
+            v: '3.20',
+            libraries: "weather,places,geometry,visualization",
+            sensor: false,
+        });
 
+    }
+]);
+
+appControllers.controller('demoCtrl', ['$scope','$timeout','$routeParams','uiGmapGoogleMapApi',
+  function($scope, $timeout, $routeParams,uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.then(function(maps) {
+      alert("maps ready");
+    });
 }]);
